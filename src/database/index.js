c147f01f129241252293edce99a839ebae11d1e1
RennanD <rplaykl@gmail.com>
2020-07-06 12:21:49 -0300

    feat: deploy

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const {connect} = require('mongoose');
 
-const database = connect('mongodb://localhost:27017/test2', {
+const database = connect(process.env.MONGODB, {
   useFindAndModify: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
